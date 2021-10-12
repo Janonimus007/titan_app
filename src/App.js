e06606e5fec4189da319fbe1b58793e87f38e473
Alejandro <kano.verapad@gmail.com>
2021-10-12 20:32:37 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
+import React,{useState} from 'react';
+import logo from './logo.jpg';
 import './App.css';
+import axios from "axios"
 
 function App() {
+  let arreglo=[0,0,
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+]
+const llamadaDeTitanes =async (titanes) =>{
+  let iterador= 30
+  let diaMenor=0
+   const llamarTitanes = await (await axios.get('http://localhost:3001/users')).data
+   llamarTitanes.map(event=>{
+     let diaVisto = event[0]
+     let ultimoDiaVisto = event[1]
+     for (let i = 0; i < arreglo.length; i++) {
+     if(i>diaVisto && i<=ultimoDiaVisto){
+      let numero = arreglo[i]
+      numero =numero+1
+      arreglo[i]=numero
+     }
+     if(arreglo[i]<arreglo[i+1]){
+       diaMenor=i
+     }
+   }
+   })
+   arreglo.map(titans=>{
+     
+     console.log(`el dia ${iterador} se vieron ${titans} titanes`)
+     iterador = iterador +1
+   })
+   console.log(diaMenor)
+  }
+
+
+  
+  
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Cuidado con los titantes</h1>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <input
+      value="llamar a los titanes"
+      onClick={llamadaDeTitanes}
+      type="submit"
+      />
+      
     </div>
   );
 }
